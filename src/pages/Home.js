fda51cd7c3a5eadcb9286febaa1728ee42e14777
Slava <slasla@gmail.com>
2022-12-22 15:31:45 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ export const Home = () => {
   let navigate = useNavigate();
   return (
     <div>
-      <h1>This Is The Homepage</h1>;
+      <h1>This Is The Homepage</h1>
       <button onClick={() => navigate("/contact")}>Go to Contact Page</button>
     </div>
   );
